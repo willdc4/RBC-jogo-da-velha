b28ec3e619093b2fe0fe254e24ae98eb47965bb2
Pedro Henrique Costa <pereke007@gmail.com>
2019-06-04 17:40:28 -0300

    Cout do tabuleiros

---
+++ base.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	char a[] = {'O', 'X', 'O', ' ', 'O', ' ', ' ', ' ', 'O'};
+	cout << "  " <<  a[0] << " | " << a[1] << " | " << a[2] << endl << " ––––––––––" << endl << " " << a[3] << "  | " << a[4] << " | " << a[5] << endl << " ––––––––––" << endl << a[6] << "   | " << a[7] << " | " << a[8];
+}
